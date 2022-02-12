7dc3c37daf25a7d98abfdddf5328a65c9a89172e
n3k <n3k1990@gmail.com>
2022-02-12 11:44:20 -0800

    starting with conditionals2

--- cap5/6.py
+++ cap5/6.py
@@ -2,4 +2,14 @@ f = open("c:\\Users\\enriq\\python_cstream_2022\\cap5\\passwords.txt", "rt")
 content = f.read()
 f.close()
 
-print(content)
+#print(type(content))
+word_list = content.split("\n")
+#print(word_list)
+
+list_of_words_with_e = []
+for word in word_list:
+    if "e" in word:
+        list_of_words_with_e.append(word.upper())
+
+print(list_of_words_with_e, len(list_of_words_with_e))
+
